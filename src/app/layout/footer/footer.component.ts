04ae2a1556f4e55e4783dda75d6e7ed0d6a0d43d
Mohamed  Ramadan <mohamedramadan9771@gmail.com>
2026-02-06 22:21:45 +0200

    nav lang. change

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { AppImgComponent } from '../../shared/components/app-img/app-img.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink, AppImgComponent],
+  imports: [CommonModule, RouterLink, TranslateModule, AppImgComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
